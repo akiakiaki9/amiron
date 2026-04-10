0a77a2a84ace5c2cffeafb0f8d1e835b4a1fb7c1
Akbar <tillayevakbarshoh@gmail.com>
2026-04-10 17:42:09 +0500

    d

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',  // Включаем статический экспорт для Netlify
+  images: {
+    unoptimized: true, // Отключаем оптимизацию изображений для статики
+  },
+  trailingSlash: true, // Добавляем слеши в конце URL
+  distDir: 'out', // Папка для экспорта (опционально, по умолчанию 'out')
 };
 
 export default nextConfig;
